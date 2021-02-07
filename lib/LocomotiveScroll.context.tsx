3ac0e7eee2d28a0613fa958fdba80f254d8f9c30
Antoine <contact@antoinelin.com>
2021-02-07 21:19:42 +0100

    revert(use-resize-observer): finaly force the use of observer to avoid weird behaviours

--- lib/LocomotiveScroll.context.tsx
+++ lib/LocomotiveScroll.context.tsx
@@ -1,5 +1,6 @@
 import { LocomotiveScrollOptions, Scroll } from 'locomotive-scroll'
-import { createContext, DependencyList, useEffect, useRef, useState } from 'react'
+import { createContext, DependencyList, MutableRefObject, useEffect, useRef, useState } from 'react'
+import useResizeObserver from 'use-resize-observer'
 
 export interface LocomotiveScrollContextValue {
   scroll: Scroll | null
@@ -13,20 +14,23 @@ export const LocomotiveScrollContext = createContext<LocomotiveScrollContextValu
 
 export interface LocomotiveScrollProviderProps {
   options: LocomotiveScrollOptions
-  watch?: DependencyList
+  containerRef: MutableRefObject<HTMLDivElement | null>
+  watch: DependencyList | undefined
 }
 
 export function LocomotiveScrollProvider({
   children,
   options,
+  containerRef,
   watch,
 }: WithChildren<LocomotiveScrollProviderProps>) {
+  const { height } = useResizeObserver<HTMLDivElement>({ ref: containerRef })
   const [isReady, setIsReady] = useState(false)
   const LocomotiveScrollRef = useRef<Scroll | null>(null)
 
   if (!watch) {
     console.warn(
-      'react-locomotive-scroll: you did not add any props to watch. Scroll may have weird behaviours if the instance is not updated when the route changes or the document height changes.'
+      'react-locomotive-scroll: you did not add any props to watch. Scroll may have weird behaviours if the instance is not updated when the route changes'
     )
   }
 
@@ -39,7 +43,7 @@ export function LocomotiveScrollProvider({
 
         if (!dataScrollContainer) {
           console.warn(
-            'react-locomotive-scroll: [data-scroll-container] dataset was not found. You likely forgot to add it which will prevent Locomotive Scroll to work.'
+            `react-locomotive-scroll: [data-scroll-container] dataset was not found. You likely forgot to add it which will prevent Locomotive Scroll to work.`
           )
         }
 
@@ -60,9 +64,12 @@ export function LocomotiveScrollProvider({
     }
   }, [])
 
-  useEffect(() => {
-    LocomotiveScrollRef.current?.update()
-  }, watch)
+  useEffect(
+    () => {
+      LocomotiveScrollRef.current?.update()
+    },
+    watch ? [...watch, height] : [height]
+  )
 
   return (
     <LocomotiveScrollContext.Provider value={{ scroll: LocomotiveScrollRef.current, isReady }}>
